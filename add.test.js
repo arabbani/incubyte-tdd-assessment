a8558cb5d4b183ef9214ff5e18acf98d2abde940
Arif Rabbani <arif.93rabbani@gmail.com>
2024-10-15 13:19:15 +0530

    test to handle single number

--- add.test.js
+++ add.test.js
@@ -4,4 +4,8 @@ describe("String Calculator", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  it("should return the number when only one number is passed", () => {
+    expect(add("5")).toBe(5);
+  });
 });
