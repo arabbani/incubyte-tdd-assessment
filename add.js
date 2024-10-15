fc25582db55d617f305ee30994040b43eba24274
Arif Rabbani <arif.93rabbani@gmail.com>
2024-10-15 13:29:35 +0530

    handle custom delimiters

--- add.js
+++ add.js
@@ -3,7 +3,16 @@ export function add(numbersString) {
     return 0;
   }
 
-  const numbersArray = numbersString.split(/[\n,]/).map(Number);
+  let delimiter = /[\n,]/;
+  let numbersToAdd = numbersString;
+
+  if (numbersString.startsWith("//")) {
+    const delimiterEndIndex = numbersString.indexOf("\n");
+    delimiter = new RegExp(numbersString.substring(2, delimiterEndIndex));
+    numbersToAdd = numbersString.substring(delimiterEndIndex + 1);
+  }
+
+  const numbersArray = numbersToAdd.split(delimiter).map(Number);
 
   return numbersArray.reduce((sum, num) => sum + num, 0);
 }
